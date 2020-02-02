6a1f0450ae15d4ba51207613e02d377e25f55590
Albert LaQue <albertlaque@gmail.com>
2020-02-02 20:43:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,5 +20,5 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each {|badge| puts badge}
-  assign_rooms(array).each {|rooms| puts rooms}
+  assign_rooms(attendees).each {|rooms| puts rooms}
   end 
