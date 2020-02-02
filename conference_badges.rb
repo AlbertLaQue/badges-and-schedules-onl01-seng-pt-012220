f4315bda05d0dc1904478ffd73eae9792a015800
Albert LaQue <albertlaque@gmail.com>
2020-02-02 20:19:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+# Each badge should read "Hello, my name is _____." 
+# Takes an array of names as an argument and returns an array of badge messages 
+
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+  array.collect do |x| 
+   "Hello, my name is #{x}."
+  end 
+end 
+
+def assign_rooms(array)
+  new_array = []
+  array.each_with_index do |x, y| 
+    new_array << "Hello, #{x}! You'll be assigned to room #{y+1}!" 
+  end 
+  new_array
+end 
+
+def printer(attendees)
+  batch_badge_creator do |x|
+     "#{x}"
+    end
+end 
