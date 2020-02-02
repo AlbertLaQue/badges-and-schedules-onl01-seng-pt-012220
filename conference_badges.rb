5e9f8323e8c1b07effd4ee69b03840ba189c78e2
Albert LaQue <albertlaque@gmail.com>
2020-02-02 20:37:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,13 +10,13 @@ def batch_badge_creator(names)
   names.map {|name| badge_maker(name)}
 end 
 
-# def assign_rooms(array)
-#   new_array = []
-#   array.each_with_index do |x, y| 
-#     new_array << "Hello, #{x}! You'll be assigned to room #{y+1}!" 
-#   end 
-#   new_array
-# end 
+def assign_rooms(array)
+  new_array = []
+  array.each_with_index do |x, y| 
+    new_array << "Hello, #{x}! You'll be assigned to room #{y+1}!" 
+  end 
+  new_array
+end 
 
 # def printer(attendees)
 #   batch_badge_creator(array).each {|x| puts x}
